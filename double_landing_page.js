a3cdacd722bc04219d763a88def151e9f1736662
Achyutha Reddy <achyuthareddyborampeta@gmail.com>
2024-09-28 09:04:13 +0530

    Add files via upload

---
+++ double_landing_page.js
@@ -0,0 +1,19 @@
+const conatinerE1=document.querySelector(".conatiner");
+const leftE1 =document.querySelector(".left");
+const rightE1=document.querySelector(".right");
+
+leftE1.addEventListener("mouseenter",()=>{
+    conatinerE1.classList.add("active-left")
+})
+
+leftE1.addEventListener("mouseleave",()=>{
+    conatinerE1.classList.remove("active-left");
+});
+
+rightE1.addEventListener("mouseenter",()=>{
+    conatinerE1.classList.add("active-right");
+});
+
+rightE1.addEventListener("mouseleave",()=>{
+    conatinerE1.classList.remove("active-right")
+})
